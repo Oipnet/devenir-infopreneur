edbe4f8c7469b00a5de2713b1d04a72ff27065b8
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-20 13:24:20 +0200

    Add favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,45 @@
         async: true,
         crossorigin: 'anonymous'
       }
-    ]
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        href: '/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-16x16.png',
+        sizes: '16x16',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
+    meta: [
+      {
+        name: 'msapplication-TileColor',
+        content: '#da532c',
+      },
+      {
+        name: 'theme-color',
+        content: '#fae9e3',
+      },
+    ],
   })
 </script>
 
