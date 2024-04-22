9ed5fc375b9a83efb8fc52a42bf9dcea90a047fb
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-22 09:13:23 +0200

    Fix contact informations

--- components/contact/ContactSummary.vue
+++ components/contact/ContactSummary.vue
@@ -7,19 +7,19 @@
     <div class="flex flex-col w-full md:w-1/3 p-4">
       <UIcon name="i-heroicons-phone" class="m-auto mb-8 text-4xl text-primary"/>
       <h2 class="text-primary text-2xl font-bold font-dancing-script mb-4">Téléphone</h2>
-      <a href="tel:0679660240" class="hover:underline">06 79 66 02 40</a>
+      <a href="tel:0679660240" class="hover:underline">07 51 67 32 49</a>
     </div>
     <div class="flex flex-col w-full md:w-1/3 p-4">
       <UIcon name="i-heroicons-map-pin" class="m-auto mb-8 text-4xl text-primary"/>
       <h2 class="text-primary text-2xl font-bold font-dancing-script mb-4">Adresse</h2>
       <address>
-        10 Hallet Sud 33720 Barsac
+        7 rue des docteurs Théry - 33210 Langon
       </address>
     </div>
     <div class="flex flex-col w-full md:w-1/3 p-4">
       <UIcon name="i-heroicons-envelope-open" class="m-auto mb-8 text-4xl text-primary"/>
       <h2 class="text-primary text-2xl font-bold font-dancing-script mb-4">E-mail</h2>
-      <a href="mailto:accompagnementsacre@gmail.com">accompagnementsacre@gmail.com</a>
+      <a href="mailto:accompagnementsacre@gmail.com">contact@forelse.info</a>
     </div>
 <!--    <div class="flex flex-col w-full md:w-1/3 p-4">-->
 <!--      <UIcon name="i-heroicons-globe-alt" class="m-auto mb-8 text-4xl text-primary"/>-->
