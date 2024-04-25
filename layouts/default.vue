0982c2d3c7b301671fde7d9ef4ebaf1d5c39ae9a
Arnaud POINTET <arnaud.pointet@gmail.com>
2024-04-25 09:46:52 +0200

    Add canonical

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,61 @@
 <script setup lang="ts">
-  useHead({
-    htmlAttrs: {
-      lang: 'fr'
-    },
-    script: [
-      {
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2253458813738703',
-        async: true,
-        crossorigin: 'anonymous'
-      }
-    ],
-    link: [
-      {
-        rel: 'apple-touch-icon',
-        href: '/apple-touch-icon.png',
-        sizes: '180x180',
+  const route = useRoute()
+
+  useHead(() => {
+    return {
+      htmlAttrs: {
+        lang: 'fr'
       },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        href: '/favicon-32x32.png',
-        sizes: '32x32',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        href: '/favicon-16x16.png',
-        sizes: '16x16',
-      },
-      {
-        rel: 'manifest',
-        href: '/site.webmanifest',
-      },
-      {
-        rel: 'mask-icon',
-        href: '/safari-pinned-tab.svg',
-        color: '#5bbad5',
-      },
-    ],
-    meta: [
-      {
-        name: 'msapplication-TileColor',
-        content: '#5bbad5',
-      },
-      {
-        name: 'theme-color',
-        content: '#5bbad5',
-      },
-    ],
+      script: [
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2253458813738703',
+          async: true,
+          crossorigin: 'anonymous'
+        }
+      ],
+      link: [
+        {
+          rel: 'canonical',
+          href: 'https://www.devenir-infopreneur.net' + route.path,
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+          sizes: '180x180',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-32x32.png',
+          sizes: '32x32',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-16x16.png',
+          sizes: '16x16',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
+        {
+          rel: 'mask-icon',
+          href: '/safari-pinned-tab.svg',
+          color: '#5bbad5',
+        },
+      ],
+      meta: [
+        {
+          name: 'msapplication-TileColor',
+          content: '#5bbad5',
+        },
+        {
+          name: 'theme-color',
+          content: '#5bbad5',
+        },
+      ],
+    }
   })
 </script>
 
